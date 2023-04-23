25738c0f26df4d440b6c36dbd383ca9ed60669de
micheleishere <131386962+micheleishere@users.noreply.github.com>
2023-04-23 20:38:27 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -16,7 +16,7 @@ st.sidebar.success("https://de.wikibooks.org/wiki/Formelsammlung_Chemie/_St%C3%B
 
 
 # Seitenleiste-Kommentar erstellen
-st.sidebar.success("Madelaine ist eine Bitch")
+st.sidebar.success("Tab 2")
 
 # Kolone erstellen, um den Titel links zu setzen und nicht in der Mitte
 col1, col2, col3 = st.columns([1,2,1])
