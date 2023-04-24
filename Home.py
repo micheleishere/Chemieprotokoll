efeb31336cdbe1ee01500ba3d599a39139a7b147
micheleishere <131386962+micheleishere@users.noreply.github.com>
2023-04-24 16:08:20 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -14,7 +14,37 @@ import streamlit as st
 # Seitenleiste-Kommentar erstellen
 st.sidebar.success("Tab 1")
 
+import streamlit as st
+import pandas as pd
+from io import StringIO
 
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
+Kopieren
+Fügen Sie einen Datei-Uploader ein, der mehrere Dateien gleichzeitig akzeptiert:
+
+import streamlit as st
+
+uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
 
 # Kolone erstellen, um den Titel links zu setzen und nicht in der Mitte
 col1, col2, col3 = st.columns([1,2,1])
